8613cc1c13a2b856a4b54875e2db1f31d09b71f1
L <1053801783@qq.com>
2018-03-21 21:08:10 +0800

    test

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import './App.scss';
 // 获取画廊数据
 const imgData = require('./data/gallery_data.json');
 // 图片所在文件夹
-// const imgDirectory = "./img/gallery_img/";
+const imgDirectory = "./img/gallery_img/";
 // 获取图片的Url
 const galleryImgs = (function getImgUrl(imgDataArr) {
   for (let i = 0, j = imgDataArr.length; i < j; i++) {
     var imgUrl = imgDataArr[i];
-    imgUrl.imgUrl = require('./img/gallery_img/' + imgDataArr[i].fileName);
+    // imgUrl.imgUrl = require('./img/gallery_img/' + imgDataArr[i].fileName);
+    imgUrl.imgUrl = require(imgDirectory + imgDataArr[i].fileName);
+    
     imgDataArr[i] = imgUrl;
   }   
   return imgDataArr;
--- src/component/imgFigure.jsx
+++ src/component/imgFigure.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 export class ImgFigure extends Component {
     render() {
+      
       return (
         <figure className="img-figure">
           <img src={this.props.data.imgUrl} alt={this.props.data.title} />
